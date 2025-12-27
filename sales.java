36d7aade23d391f1bdda2756abcd074e3bc7cdca
nimal <nimal@gmail.com>
2025-12-27 12:32:54 +0000

    Added New Code

--- sales.java
+++ sales.java
@@ -4,3 +4,6 @@ public class Main {
   }
 }
 System.out.println("Hello World!");
+System.out.println("Hello World!");
+System.out.println("I am learning Java.");
+System.out.println("It is awesome!");
